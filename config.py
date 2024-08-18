5cfbb7a31170a30143c95a7975afea72e220f1e3
manish8000 <147980408+manish8000@users.noreply.github.com>
2024-08-18 18:26:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,4 +10,4 @@ API_ID = int(os.environ.get("API_ID", "24594907"))
 API_HASH = os.environ.get("API_HASH", "cebf6f41df2f1cfe56e25beac945ac36")
 
 #Database 
-DB_URI = os.environ.get("DB_URI", "http://92@cluster0.n0zhthj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = os.environ.get("DB_URI", "mongodb+srv://manishbairwa9632:Manish@92@cluster0.n0zhthj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
